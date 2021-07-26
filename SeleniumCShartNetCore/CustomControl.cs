fb6e6b75dab5d209d01e708b6a3093225cc2d5c1
Dona Dassanayake <5779047+DonaDassanayake@users.noreply.github.com>
2021-07-26 19:47:43 +1200

    Initial commit until Combo control

---
+++ SeleniumCShartNetCore/CustomControl.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumCShartNetCore
+{
+    public class CustomControl: DriverHelper
+    {
+        public static void ComboBox(string controlName, string value)
+        {
+            IWebElement comboControl = Driver.FindElement(By.XPath($"//input[@id='{controlName}-awed']"));
+            
+            comboControl.Clear();
+            comboControl.SendKeys(value);
+            //Click the typed value from the combo box
+            Driver.FindElement(By.XPath($"//div[@id = '{controlName}-dropmenu']//li[text()='{value}']")).Click();
+
+        }
+    }
+}
---
+++ SeleniumCShartNetCore/UnitTest1.cs
@@ -0,0 +1,46 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace SeleniumCShartNetCore
+{
+    public class Tests: DriverHelper
+    {
+        //public static IWebDriver Driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            Console.WriteLine("Setup");
+            Driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Driver.Navigate().GoToUrl("https://demowf.aspnetawesome.com/");
+
+            //Type into a tetxtboxUnit Testing
+            Driver.FindElement(By.Id("ContentPlaceHolder1_Meal")).SendKeys("Tomato");
+            
+            //Checkbox
+            Driver.FindElement(By.XPath("//input[@name='ctl00$ContentPlaceHolder1$ChildMeal1']/following-sibling::div[text()='Celery']")).Click();
+
+            /*
+            //Combo box
+            IWebElement comboControl = Driver.FindElement(By.XPath("//input[@id='ContentPlaceHolder1_AllMealsCombo-awed']"));
+            comboControl.Clear();
+            comboControl.SendKeys("Almond");
+            //Click the typed value from the combo box
+            Driver.FindElement(By.XPath("//div[@id = 'ContentPlaceHolder1_AllMealsCombo-dropmenu']//li[text()='Almond']")).Click();
+            */
+            string comboControlName = "ContentPlaceHolder1_AllMealsCombo";
+            //CustomControl control = new CustomControl();
+            CustomControl.ComboBox(comboControlName, "Almond");
+
+            Console.WriteLine("Test1");
+            Assert.Pass("Passed");
+        }
+    }
+}
